279ff79a7fe488a9ddaed3ac85fb82f835d07cbd
Jared Stull <jstull29@hotmail.com>
2022-05-09 16:54:11 -0500

    books route added

--- index.js
+++ index.js
@@ -19,3 +19,55 @@ mongoose.connect(
   "mongodb+srv://jstull29:4mS69fOTPJe6x1T1@book.ezg04.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { dbName: "bookDB", useNewUrlParser: true }
 );
+
+const bookSchema = {
+    title: String,
+    author: String,
+    start: String,
+    finish: String,
+    chapters: Number
+};
+
+const Book = mongoose.model("Book", bookSchema);
+
+// Target all books
+app
+    .route("/books")
+    .get(function (req, res) {
+        Book.find(function (err, foundBooks) {
+            if(!err) {
+                res.send(foundBooks);
+            }
+            else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newBook = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            start: req.body.start,
+            finish: req.body.finish,
+            chapters: req.body.chapters
+        });
+
+        newBook.save(function (err) {
+            if(!err) {
+                res.send("Book added successfully.");
+            }
+            else{
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Book.deleteMany(function (err) {
+            if(!err) {
+                res.send("All books deleted.")
+            }
+            else {
+                res.send(err);
+            }
+        });
+    });
